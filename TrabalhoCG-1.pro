95973b420d4c6a32c12a3b5e7e0c038b533cad1f
06457680305 <anderson.asm@alu.ufc.br>
2021-06-27 10:30:03 -0300

    Initial commit

---
+++ TrabalhoCG-1.pro
@@ -0,0 +1,53 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+INCLUDEPATH += bib
+INCLUDEPATH += gui_glut
+INCLUDEPATH += pessoal
+
+LIBS += -lGL -lGLU -lglut -l3ds -lSDL_image
+
+SOURCES += main.cpp \
+    bib/Camera.cpp \
+    bib/CameraDistante.cpp \
+    bib/CameraJogo.cpp \
+    bib/Desenha.cpp \
+    bib/model3ds.cpp \
+    bib/Vetor3D.cpp \
+    gui_glut/extra.cpp \
+    gui_glut/gui.cpp \
+    pessoal/casa.cpp \
+    pessoal/personagem.cpp \
+    pessoal/objeto.cpp \
+    pessoal/carro.cpp \
+    pessoal/novoobjeto.cpp \
+    imgui/imgui.cpp \
+    imgui/imgui_demo.cpp \
+    imgui/imgui_draw.cpp \
+    imgui/imgui_widgets.cpp \
+    imgui/examples/imgui_impl_glut.cpp \
+    imgui/examples/imgui_impl_opengl2.cpp
+
+
+HEADERS += \
+    bib/Camera.h \
+    bib/CameraDistante.h \
+    bib/CameraJogo.h \
+    bib/Desenha.h \
+    bib/model3ds.h \
+    bib/Vetor3D.h \
+    gui_glut/extra.h \
+    gui_glut/gui.h \
+    pessoal/casa.h \
+    pessoal/personagem.h \
+    pessoal/objeto.h \
+    pessoal/carro.h \
+    pessoal/novoobjeto.h \
+    imgui/imgui.h \
+    imgui/imgui_demo.h \
+    imgui/imgui_draw.h \
+    imgui/imgui_widgets.h \
+    imgui/examples/imgui_impl_glut.h
+    imgui/examples/imgui_impl_opengl2.h
---
+++ main.cpp
@@ -0,0 +1,276 @@
+#include <iostream>
+
+using namespace std;
+
+//#include <gui.h>
+#include <vector>
+
+//imgui includes
+#include "imgui/imgui.h"
+#include "imgui/examples/imgui_impl_glut.h"
+#include "imgui/examples/imgui_impl_opengl2.h"
+
+//#include <objeto.h>
+#include <personagem.h>
+#include <casa.h>
+#include <carro.h>
+#include <novoobjeto.h>
+
+//Model3DS model3ds("../3ds/cartest.3DS");
+
+vector<Objeto*> objetos;
+int posSelecionado = -1;
+
+// Our state
+static bool show_demo_window = false;
+static bool show_another_window = false;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+void imgui_display()
+{
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            //ShowExampleMenuFile();
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Edit"))
+        {
+            if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+            if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+            ImGui::Separator();
+            if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+            if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+            if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGuiIO& io = ImGui::GetIO();
+        io.ConfigWindowsMoveFromTitleBarOnly = true;
+
+        ImGui::Begin("Stats", NULL);
+        ImGui::SetWindowSize(ImVec2(glutGet(GLUT_WINDOW_WIDTH), 200));
+        ImGui::SetWindowPos(ImVec2(0, glutGet(GLUT_WINDOW_HEIGHT) - ImGui::GetWindowHeight()));
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+}
+
+Vetor3D transformedPoint(Vetor3D p)
+{
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+        glLoadIdentity();
+
+        glRotatef(objetos[0]->a.z,0,0,1);  // \ .
+        glRotatef(objetos[0]->a.y,0,1,0);  //  | Rz.Ry.Rx . v
+        glRotatef(objetos[0]->a.x,1,0,0);
+
+        float matrix[16];
+        glGetFloatv(GL_MODELVIEW_MATRIX, matrix);
+    glPopMatrix();
+
+    float pos[4] = {p.x,p.y,p.z, 1.0};
+    float res[4];
+
+    glutGUI::multGLMatrixByVector(res,matrix,pos);
+
+    Vetor3D res3D = Vetor3D(res[0],res[1],res[2]);
+    return res3D;
+}
+
+void displayInner() {
+    GUI::setLight(1,1,3,5,true,false);
+    GUI::setLight(2,-1.5,0.5,-1,true,false);
+    //GUI::setLight(3,-5,3,5,true,false);
+
+    GUI::drawOrigin(1);
+
+    GUI::setColor(1,0,0);
+    GUI::drawFloor();
+
+    for (int i = 0; i < objetos.size(); ++i) {
+        glPushMatrix();
+            objetos[i]->desenha();
+        glPopMatrix();
+    }
+}
+
+void desenha() {
+    GUI::displayInit();
+
+    // Start the Dear ImGui frame
+
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+    imgui_display();
+
+    // Rendering
+    ImGui::Render();
+    ImGuiIO& io = ImGui::GetIO();
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+
+
+    gluLookAt(glutGUI::cam->e.x,glutGUI::cam->e.y,glutGUI::cam->e.z, glutGUI::cam->c.x,glutGUI::cam->c.y,glutGUI::cam->c.z, glutGUI::cam->u.x,glutGUI::cam->u.y,glutGUI::cam->u.z);
+    //gluLookAt(0,15,0, 0,0,0, 1,0,0);
+    displayInner();
+
+
+    if (!objetos.empty()) {
+        glLoadIdentity();
+        glViewport(0, 3*glutGUI::height/4, glutGUI::width/4, glutGUI::height/4);
+        Vetor3D zlocalemCoordsGlobais = transformedPoint(Vetor3D(0,0,1));
+        Vetor3D olho = objetos[0]->t + zlocalemCoordsGlobais*1.2;
+        Vetor3D centro = olho + zlocalemCoordsGlobais;
+
+        Vetor3D ylocalemCoordsGlobais = transformedPoint(Vetor3D(0,1,0));
+        Vetor3D up = ylocalemCoordsGlobais;
+
+        gluLookAt(olho.x,olho.y,olho.z, centro.x,centro.y,centro.z, up.x,up.y,up.z);
+        displayInner();
+    }
+
+
+    if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
+        objetos[posSelecionado]->t.x += glutGUI::dtx;
+        objetos[posSelecionado]->t.y += glutGUI::dty;
+        objetos[posSelecionado]->t.z += glutGUI::dtz;
+
+        objetos[posSelecionado]->a.x += glutGUI::dax;
+        objetos[posSelecionado]->a.y += glutGUI::day;
+        objetos[posSelecionado]->a.z += glutGUI::daz;
+
+        objetos[posSelecionado]->s.x += glutGUI::dsx;
+        objetos[posSelecionado]->s.y += glutGUI::dsy;
+        objetos[posSelecionado]->s.z += glutGUI::dsz;
+    }
+
+
+    glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
+    //glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+    //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+
+
+    GUI::displayEnd();
+    glutPostRedisplay();
+}
+
+bool incluirObjeto = false;
+
+void teclado(unsigned char key, int x, int y) {
+    //if (!incluirObjeto) {
+        GUI::keyInit(key,x,y);
+    //}
+
+    switch (key) {
+    case 't':
+        glutGUI::trans_obj = !glutGUI::trans_obj;
+        break;
+    case 'l':
+        glutGUI::trans_luz = !glutGUI::trans_luz;
+        break;
+
+    case 'n':
+        if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
+            objetos[posSelecionado]->selecionado = false;
+        }
+        posSelecionado++;
+        posSelecionado = posSelecionado%objetos.size();
+        if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
+            objetos[posSelecionado]->selecionado = true;
+        }
+        break;
+    case 'b':
+        if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
+            objetos[posSelecionado]->selecionado = false;
+        }
+        posSelecionado--;
+        if (posSelecionado < 0) {
+            posSelecionado = objetos.size()-1;
+        }
+        if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
+            objetos[posSelecionado]->selecionado = true;
+        }
+        break;
+
+
+    case 'O':
+        incluirObjeto = !incluirObjeto;
+        break;
+    case 'p':
+        if (incluirObjeto) {
+            objetos.push_back( new Personagem() );
+        }
+        break;
+    case 'c':
+        if (incluirObjeto) {
+            objetos.push_back( new Carro() );
+        }
+        break;
+    case 'C':
+        if (incluirObjeto) {
+            objetos.push_back( new Casa() );
+        }
+        break;
+    case '4':
+        if (incluirObjeto) {
+            objetos.push_back( new novoObjeto() );
+        }
+        break;
+    case 'i':
+        if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
+            objetos[posSelecionado]->t.x = 0.0;
+            objetos[posSelecionado]->t.y = 0.0;
+            objetos[posSelecionado]->t.z = 0.0;
+
+            objetos[posSelecionado]->a.x += glutGUI::dax;
+            objetos[posSelecionado]->a.y += glutGUI::day;
+            objetos[posSelecionado]->a.z += glutGUI::daz;
+
+            objetos[posSelecionado]->s.x += glutGUI::dsx;
+            objetos[posSelecionado]->s.y += glutGUI::dsy;
+            objetos[posSelecionado]->s.z += glutGUI::dsz;
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    GUI gui = GUI(800,600,desenha,teclado);
+}
